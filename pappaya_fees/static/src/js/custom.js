10ed325a91a4dd0a31da2aa1ff4b3ca422c4d47b
tangaraj <tangaraj@think42labs.com>
2018-06-22 12:58:34 +0530

    Remove datafds

--- pappaya_fees/static/src/js/custom.js
+++
@@ -1,9 +0,0 @@
-$('button.btn.btn-sm.oe_button.oe_highlight').click(function(e)
-{
-    // Special stuff to do when this link is clicked...
-
-    // Cancel the default action
-    e.preventDefault();
-});
-
-
